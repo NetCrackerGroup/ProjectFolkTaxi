151161888a939f24c8f5b78085ed79ebda3dcff4
Nikolay <murzinov.nikolay@yandex.ru>
2019-12-19 16:16:01 +0300

    City, User, DriverRating, Group, PassengerRating,Review 2

---
+++ src/main/java/com/netcracker/repositories/GroupRepository.java
@@ -0,0 +1,10 @@
+package com.netcracker.repositories;
+
+import com.netcracker.entities.Group;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepository extends CrudRepository<Group, Long> {
+
+}
