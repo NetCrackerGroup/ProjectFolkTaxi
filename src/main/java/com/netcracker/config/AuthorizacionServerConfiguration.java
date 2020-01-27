b513aa491622de680f214798093f11f5059f1b80
Nikolay <murzinov.nikolay@yandex.ru>
2020-01-28 00:40:01 +0300

    Aouth2 part2

--- src/main/java/com/netcracker/config/AuthorizacionServerConfiguration.java
+++ src/main/java/com/netcracker/config/AuthorizacionServerConfiguration.java
@@ -1,19 +1,27 @@
 package com.netcracker.config;
 
+import com.netcracker.security.services.UserSpringDetailsService;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableAuthorizationServer
@@ -24,13 +32,15 @@ public class AuthorizacionServerConfiguration  extends AuthorizationServerConfig
     private AuthenticationManager authenticationManager;
 
     @Autowired
-    private TokenStore tokenStore;
+    private UserSpringDetailsService userDetailsService;
+
+    @Autowired
+    private DataSource dataSource;
 
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-
-        clients.inMemory()
+        clients.jdbc(dataSource)
                 .withClient("client")
                 .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
                 .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT","USER", "ADMIN")
@@ -46,14 +56,49 @@ public class AuthorizacionServerConfiguration  extends AuthorizationServerConfig
     }
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints
-                .authenticationManager(authenticationManager)
-                .tokenStore(tokenStore);
+        endpoints.userDetailsService(userDetailsService)
+                .authorizationCodeServices(authorizationCodeServices())
+                .authenticationManager(this.authenticationManager)
+                .tokenStore(tokenStore()).approvalStoreDisabled();
     }
 
     @Bean
-    public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
+    public JdbcTokenStore  tokenStore() {
+        return new JdbcTokenStores(dataSource);
     }
-//
+    @Bean
+    protected AuthorizationCodeServices authorizationCodeServices() {
+        return new JdbcAuthorizationCodeServices(dataSource);
+    }
+
+
 }
+ class JdbcTokenStores extends JdbcTokenStore {
+    private static final Log LOG = LogFactory.getLog(JdbcTokenStores.class);
+
+    public JdbcTokenStores(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public OAuth2AccessToken readAccessToken(String tokenValue) {
+        OAuth2AccessToken accessToken = null;
+
+        try {
+            accessToken = new DefaultOAuth2AccessToken(tokenValue);
+        }
+        catch (EmptyResultDataAccessException e) {
+            if (LOG.isInfoEnabled()) {
+                LOG.info("Failed to find access token for token "+tokenValue);
+            }
+        }
+        catch (IllegalArgumentException e) {
+            LOG.warn("Failed to deserialize access token for " +tokenValue,e);
+            removeAccessToken(tokenValue);
+        }
+
+        return accessToken;
+    }
+}
+
+
