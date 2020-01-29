b1634f6dfa5f8420c885c5272da96165bbf7aa8a
Nikolay <murzinov.nikolay@yandex.ru>
2020-01-29 22:29:06 +0300

    Aouth2 part2

--- src/main/resources/db/migration/V1.1__insert_date.sql
+++ src/main/resources/db/migration/V1.1__insert_date.sql
@@ -8,3 +8,5 @@ INSERT INTO Report(Report_ID , Report_Text, Report_Reason, Was_Considered, Moder
 INSERT INTO Report(Report_ID , Report_Text, Report_Reason, Was_Considered, Moderator_ID, User_ID)  VALUES (2 , 'aaaaa', 'ooooo', false, 1, 1);
 
 INSERT INTO Notification(Notification_ID, Text, Delivery_Channel, Was_Watched, User_ID) VALUES (1 , 'Завтра едешь?' , 'Голубиная почта' , false , 1);
+
+INSERT INTO oauth_client_details (client_id, client_secret, web_server_redirect_uri, scope, access_token_validity, refresh_token_validity, resource_ids, authorized_grant_types, additional_information) VALUES ('mobile', '$2a$10$gPhlXZfms0EpNHX0.HHptOhoFD1AoxSr/yUIdTqA8vtjeP4zi0DDu', 'http://localhost:1337/code', 'READ,WRITE', '3600', '10000', 'inventory,payment', 'authorization_code,password,refresh_token,implicit', '{}');
