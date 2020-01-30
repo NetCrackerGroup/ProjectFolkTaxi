dcb678d5250d6361d426244215a22be18622ec36
Nikolay <murzinov.nikolay@yandex.ru>
2020-01-30 15:18:50 +0300

    30.1 oauth2

--- src/main/java/com/netcracker/config/WebSecurityConfiguration.java
+++ src/main/java/com/netcracker/config/WebSecurityConfiguration.java
@@ -1,1 +1,52 @@
-//
+package com.netcracker.config;
+
+import com.netcracker.security.services.UserSpringDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Autowired
+    private UserSpringDetailsService userSpringDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userSpringDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/users/helloUser").permitAll()
+                .antMatchers().authenticated()
+                .antMatchers( "/users/User").hasRole("USER")
+                .antMatchers( "/users/Admin").hasRole("ADMIN")
+                .and().rememberMe()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout() // Metodo get pues he desabilitado CSRF
+                .permitAll();
+    }
+
+
+}
--- src/main/resources/db/migration/V1.0__init_schema.sql
+++ src/main/resources/db/migration/V1.0__init_schema.sql
@@ -61,78 +61,8 @@ CREATE TABLE public.User_1 (
                 CONSTRAINT user_id PRIMARY KEY (User_ID)
 );
 
--- CREATE SEQUENCE role_id_seq;
---
--- CREATE TABLE public.Role_1 (
---                Role_ID BIGINT NOT NULL default nextval('role_id_seq'),
---                User_ID BIGINT NOT NULL,
---                CONSTRAINT user_id PRIMARY KEY (Role_ID)
--- );
-drop table if exists oauth_client_details;
-create table oauth_client_details (
-                                      client_id VARCHAR(255) PRIMARY KEY,
-                                      resource_ids VARCHAR(255),
-                                      client_secret VARCHAR(255),
-                                      scope VARCHAR(255),
-                                      authorized_grant_types VARCHAR(255),
-                                      web_server_redirect_uri VARCHAR(255),
-                                      authorities VARCHAR(255),
-                                      access_token_validity INTEGER,
-                                      refresh_token_validity INTEGER,
-                                      additional_information VARCHAR(4096),
-                                      autoapprove VARCHAR(255)
-);
-drop table if exists oauth_client_token;
-create table oauth_client_token (
-                                    token_id VARCHAR(256),
-                                    token bytea,
-                                    authentication_id VARCHAR(256),
-                                    user_name VARCHAR(256),
-                                    client_id VARCHAR(256)
-);
-drop table if exists oauth_access_token;
-create table oauth_access_token (
-                                    token_id VARCHAR(256),
-                                    token bytea,
-                                    authentication_id VARCHAR(256),
-                                    user_name VARCHAR(256),
-                                    client_id VARCHAR(256),
-                                    authentication bytea,
-                                    refresh_token VARCHAR(256)
-);
-drop table if exists oauth_refresh_token;
-create table oauth_refresh_token (
-                                     token_id VARCHAR(255),
-                                     token bytea,
-                                     authentication bytea
-);
-drop table if exists oauth_code;
-create table oauth_code (
-                            code VARCHAR(256), authentication bytea
-);
-drop table if exists oauth_approvals;
-create table oauth_approvals (
-                                 userId VARCHAR(255),
-                                 clientId VARCHAR(255),
-                                 scope VARCHAR(255),
-                                 status VARCHAR(10),
-                                 expiresAt TIMESTAMP,
-                                 lastModifiedAt TIMESTAMP
-);
-drop table if exists ClientDetails;
-create table ClientDetails (
-                               appId VARCHAR(255) PRIMARY KEY,
-                               resourceIds VARCHAR(255),
-                               appSecret VARCHAR(255),
-                               scope VARCHAR(255),
-                               grantTypes VARCHAR(255),
-                               redirectUrl VARCHAR(255),
-                               authorities VARCHAR(255),
-                               access_token_validity INTEGER,
-                               refresh_token_validity INTEGER,
-                               additionalInformation VARCHAR(4096),
-                               autoApproveScopes VARCHAR(255)
-);
+
+
 
 CREATE SEQUENCE review_id_seq;
 
