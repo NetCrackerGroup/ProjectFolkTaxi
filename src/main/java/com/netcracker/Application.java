50cb2b84f8aed5775861fb727b4c771627d460b4
Nikolay <murzinov.nikolay@yandex.ru>
2020-02-07 11:44:18 +0300

    32

--- src/main/java/com/netcracker/Application.java
+++ src/main/java/com/netcracker/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
-public class Application {
+public class    Application {
 
     public static void main(String[] args){
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
