9baa7ab0adae31dd5f20b26df7b7317e05de6536
ElizabetDunayeva <57858485+ElizabetDunayeva@users.noreply.github.com>
2020-03-19 20:12:09 +0300

    Add files via upload

--- src/main/java/com/netcracker/repositories/ScheduleRepository.java
+++ src/main/java/com/netcracker/repositories/ScheduleRepository.java
@@ -1,10 +1,12 @@
 package com.netcracker.repositories;
 
+import com.netcracker.entities.Route;
 import com.netcracker.entities.Schedule;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScheduleRepository extends CrudRepository<Schedule, Long> {
+    Schedule findScheduleByRouteId(Route routeId);
 
 }
--- src/main/java/com/netcracker/repositories/UserRepository.java
+++ src/main/java/com/netcracker/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package com.netcracker.repositories;
 
 //import org.springframework.data.repository.CrudRepository;
+import com.netcracker.entities.Group;
+import com.netcracker.entities.Route;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,8 @@ import java.util.List;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findUserByEmail(String email);
+    List<User> findAllByRoutes(Route route);
+    List<User> findAllByGroups(Group group);
     User findUserByFio(String username);
+    User findUserByUserId(Long userId);
 }
