c46f75a59cf07ff3210b2fee5518573b097bc63e
ElizabetDunayeva <57858485+ElizabetDunayeva@users.noreply.github.com>
2020-03-19 21:49:45 +0300

    Update V1.14__alter_tables.sql

--- src/main/resources/db/migration/V1.14__alter_tables.sql
+++ src/main/resources/db/migration/V1.14__alter_tables.sql
@@ -4,5 +4,5 @@ ADD count_of_places BIGINT NOT NULL default 1;
 
 ALTER TABLE schedules
     ADD schedule_day BIGINT;
-
-   ADD start_day ALTER TABLE routes
+ALTER TABLE routes
+   ADD start_day date;
