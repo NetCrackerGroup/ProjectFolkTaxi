54f64e8fee04656ed291f8c6414c7e7c09b2b29d
Nikolay <murzinov.nikolay@yandex.ru>
2020-04-19 15:55:25 +0300

    trav deploy

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rsync -r --quiet --delete-after target/project-folk-taxi-0.0.1-SNAPSHOT.jar $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIRECTORY
-ssh $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIRECTORY mkdir "dirname"
+ssh $DEPLOY_USER@$DEPLOY_HOST mkdir "dirname"
 echo 'Hello'
 echo 'World'
