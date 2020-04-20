6becb1da8bc96cb8c9f1dd31aa1689c1ab7f5999
Nikolay <murzinov.nikolay@yandex.ru>
2020-04-20 14:51:36 +0300

    trav deploy

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-ssh $DEPLOY_USER@$DEPLOY_HOST ./upgradeScript
-echo 'Hello'
+rsync -r --quiet --delete-after target/project-folk-taxi-0.0.1-SNAPSHOT.jar $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIRECTORY
+if [ "${TRAVIS_EVENT_TYPE}" = "cron" ]; then
+  ssh $DEPLOY_USER@$DEPLOY_HOST ./upgradeScript
+  echo 'Cronn'
+fi
 echo 'World'
